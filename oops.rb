27afc93f8e3972c4bd9f53dd67bf564fef2a67a2
varunsahil10 <varunsahil10@gmail.com>
2017-06-07 06:12:25 -0400

    inheritance of classes

--- oops.rb
+++ oops.rb
@@ -1,8 +1,37 @@
 class User
+  
+  attr_accessor :name
   def initialize(name)
     @name = name
   end
+
+  def self.identify
+    puts "this is a class method"
+  end
+=begin
+ 
+  def get_name
+    @name
+  end
+  
+  def set_name=(n)
+    @name = n
+  end
+
+=end
+end
+
+class Buyer < User
+    
 end
 
 user = User.new("Varun")
 user1 = User.new("Sahil")
+puts user.name
+buyer1 = Buyer.new("Manish")
+puts buyer1.name
+#puts user1.name
+user.name = "Alwar"
+puts user.name
+puts "hey buddy"
+User.identify
